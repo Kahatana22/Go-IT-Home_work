1f965db4362b77e180b264d06ce56dc408a0da2a
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-04-17 14:48:09 +0300

    Create User.java

---
+++ src/main/java/Modul10/Tasks/Task2/User.java
@@ -0,0 +1,11 @@
+package Modul10.Tasks.Task2;
+
+public class User {
+    private final String name;
+    private final int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}

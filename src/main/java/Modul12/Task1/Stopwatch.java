238975f07e8c7bac51573d22de703c5ea882e212
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-04-30 16:00:16 +0300

    Create Stopwatch.java

---
+++ src/main/java/Modul12/Task1/Stopwatch.java
@@ -0,0 +1,54 @@
+package Modul12.Task1;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Stopwatch extends Thread {
+
+    public AtomicInteger counter = new AtomicInteger();
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(counter);
+            try {
+                Thread.sleep(1000);
+                counter.getAndIncrement();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Stopwatch sw = new Stopwatch();
+        sw.start();
+
+        while (true) {
+            Thread.sleep(5000);
+            System.out.println("Минуло 5 секунд");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
